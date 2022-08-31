b8083974bf2fea0d4448754c62df2caf918e01e4
Carlos Vivas <carlosmuerto@gmail.com>
2022-08-31 13:16:49 -0400

    Retriving coment form api

--- src/module/domService.js
+++ src/module/domService.js
@@ -1,27 +1,13 @@
+import apiService from './apiService.js';
+
+const commentUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Sff4TpvfhVNiDO6YpeWb/comments';
+
 const container = document.querySelector('.flex-container');
 
 const ShowItemTemplate = document.querySelector('.template #show-item-template');
 const ShowModalTemplate = document.querySelector('.template #show-modal-template');
 
-const buildModal = (movie) => {
-  const ShowModalElement = ShowModalTemplate.cloneNode(true);
-
-  console.log(movie);
-
-  ShowModalElement.id = `show-modal-${movie.id}`;
-
-  ShowModalElement.querySelector('span.close').addEventListener('click', () => {
-    ShowModalElement.remove();
-  });
-
-  const imgElement = document.createElement('img');
-  imgElement.src = movie.image;
-  imgElement.alt = `${movie.title} Poster`;
-
-  ShowModalElement.querySelector('.modal-detail-img').appendChild(imgElement);
-
-  const detailTextElement = ShowModalElement.querySelector('.modal-detail-text');
-
+const buildDetailTextElement = (detailTextElement, movie) => {
   const titleElement = detailTextElement.querySelector('.name');
   titleElement.textContent = movie.name;
 
@@ -39,6 +25,47 @@ const buildModal = (movie) => {
 
   const summaryElement = detailTextElement.querySelector('.summary');
   summaryElement.innerHTML = movie.summary;
+};
+
+const buildDetailcommentElement = (detailcommentElement, movie) => {
+  const comentCounter = detailcommentElement.querySelector('.comment-counter');
+  comentCounter.textContent = '( Loading... )';
+
+  const comentList = detailcommentElement.querySelector('.view-comment');
+  const LoadingdivElement = document.createElement('div');
+  LoadingdivElement.classList.add('loading-coment-item');
+  LoadingdivElement.textContent = '( Loading... )';
+
+  apiService.getComments(commentUrl, movie.id).then((CommentList) => {
+    comentList.innerHTML = '';
+    CommentList.List.forEach((comment) => {
+
+    });
+  });
+
+  comentList.appendChild(LoadingdivElement);
+};
+
+const buildModal = (movie) => {
+  const ShowModalElement = ShowModalTemplate.cloneNode(true);
+
+  ShowModalElement.id = `show-modal-${movie.id}`;
+
+  ShowModalElement.querySelector('span.close').addEventListener('click', () => {
+    ShowModalElement.remove();
+  });
+
+  const imgElement = document.createElement('img');
+  imgElement.src = movie.image;
+  imgElement.alt = `${movie.title} Poster`;
+
+  ShowModalElement.querySelector('.modal-detail-img').appendChild(imgElement);
+
+  const detailTextElement = ShowModalElement.querySelector('.modal-detail-text');
+  buildDetailTextElement(detailTextElement, movie);
+
+  const detailcommentElement = ShowModalElement.querySelector('.modal-detail-comment');
+  buildDetailcommentElement(detailcommentElement, movie);
 
   document.querySelector('body').appendChild(ShowModalElement);
 };
